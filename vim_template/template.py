801850bb12609af581ab7729a38d88c66fb5be87
pettan0818 <pettan0818+git@gmail.com>
2015-11-04 02:07:21 +0900

    [New] vim template file is now setuped.

---
+++ vim_template/template.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+"""
+#
+# Author:   Noname
+# URL:      https://github.com/pettan0818
+# License:  MIT License
+# Created:  <+DATE+>
+#
+
+# Usage
+#
+"""
+
+<+CURSOR+>
---
+++ vim_template/template.sh
@@ -0,0 +1,14 @@
+# !/bin/zsh
+
+#
+# Author:   Noname
+# URL:      http://github.com/pettan0818
+# License:  MIT License
+# Created:  <+DATE+>
+#
+
+# Usage
+#
+#
+
+<+CURSOR+>
