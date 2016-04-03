fca06c7bdc48c9e08c2173eafcf3f0f1dbda310b
pettan0818 <pettan0818+git@gmail.com>
2016-04-03 21:13:34 +0900

    [install.zsh] Now added to download tmux plugin manager.

--- install.zsh
+++ install.zsh
@@ -8,5 +8,9 @@ ln -snf ./dotfiles/.tmux.conf ./
 ln -snf ./dotfiles/.vimrc.initializer ./
 ln -snf ./dotfiles/.zshrc.zplug ./
 
+# NeoBundle installer
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > neo_bundle.sh
 sh ./neo_bundle.sh
+
+# Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
