1a91b742e3d09b761e8908c5e32037b62323fad2
pettan0818 <pettan0818+git@gmail.com>
2017-01-29 23:41:56 +0900

    [installer] python2 adobption removed.

--- utility/installer.py
+++ utility/installer.py
@@ -14,11 +14,6 @@
 # Usage
 #
 """
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import absolute_import
-
 import logging
 import os
 import platform
