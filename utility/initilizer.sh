633417a5fc1c84eb42771e9c9e6b7eb27d5d441c
pettan0818 <pettan0818+git@gmail.com>
2017-03-28 10:01:16 +0900

    [Removed pyenv virtualenv]

--- utility/initilizer.sh
+++ utility/initilizer.sh
@@ -1,5 +1,3 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-
-git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
